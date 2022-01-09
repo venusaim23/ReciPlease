56d3a4bc876de1f3d7484859ae20d24b2864a331
Venu Sai <venusaim23@gmail.com>
2022-01-09 15:21:12 +0530

    setup navigation

--- app/src/main/java/com/mlh/reciplease/MainActivity.java
+++ app/src/main/java/com/mlh/reciplease/MainActivity.java
@@ -8,8 +8,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
@@ -20,21 +22,32 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FragmentManager fragmentManager;
+    private NavHostFragment navHostFragment;
+    private NavController navController;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        appBarConfiguration = new AppBarConfiguration.Builder(R.id.navigation_home,
+                R.id.navigation_favourites, R.id.navigation_recipes).build();
+
+        fragmentManager = getSupportFragmentManager();
+        navHostFragment = (NavHostFragment) fragmentManager.findFragmentById(R.id.fragment_container_view);
+
+        if (navHostFragment != null) {
+            navController = navHostFragment.getNavController();
+//        navController = Navigation.findNavController(this, R.id.fragment_container_view);
+            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+            NavigationUI.setupWithNavController(binding.contentMain.navView, navController);
+        }
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -67,10 +80,10 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+//    @Override
+//    public boolean onSupportNavigateUp() {
+////        NavController navController = Navigation.findNavController(this, R.id.fragment_container_view);
+//        return NavigationUI.navigateUp(navController, appBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
 }
---
+++ app/src/main/java/com/mlh/reciplease/RecipesFragment.java
@@ -0,0 +1,35 @@
+package com.mlh.reciplease;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.mlh.reciplease.databinding.FragmentRecipesBinding;
+
+public class RecipesFragment extends Fragment {
+
+    private FragmentRecipesBinding binding;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentRecipesBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+}
